3d1c0bcc50d7a3fc94c38798a77e8137c4f20b91
RinaFrm <SemyonovaRina@mail.ru>
2023-03-11 12:24:44 +0300

    !

--- src/server/server.js
+++ src/server/server.js
@@ -2,12 +2,23 @@ import express from 'express';
 import ReactDOM from 'react-dom/server';
 import { App } from '../App';
 import { indexTemplate } from './indexTemplate';
-import compression 
+import compression from 'compression';
+import helmet from 'helmet';
 
 const PORT = process.env.PORT || 3000;
+const IS_DEV = process.env.NODE_ENV !== 'production';
 const app = express();
+
 //поменять ссылки переходов с локалхост
+if (!IS_DEV) {
+  app.use(compression());
+  app.use(helmet({
+    contentSecurityPolicy: false,
+  }));
+}
+
 app.use('/static', express.static('./dist/client'));
+
 app.get('/auth', (req, res) => {
   res.send(
     indexTemplate(ReactDOM.renderToString(App()), req.query.code, process.env.CLIENT_ID, process.env.SECRET),
